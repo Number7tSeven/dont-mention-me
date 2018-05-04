4c2c593a16b9370378d02a6a872216a076d79b6c
Number7tSeven <taylordcorum@gmail.com>
2018-05-04 14:13:04 -0400

    Added hello command description"

--- main.py
+++ main.py
@@ -84,6 +84,7 @@ async def on_message(message):
 
 @bot.command(aliases=['hello!', 'Hello', 'Hello!', 'Hi', 'hi', 'Hi!', 'hi!', 'wave', 'ohaiyo', 'ohaiyo!'])
 async def hello(ctx):
+    """Hi!"""
     await ctx.send('Hi! :grin:')
 
 with open('key.txt') as keyfile:
